51f58cc1007662e71ceb3f8e66e15767c074f481
khj1212k <khj1212k@naver.com>
2022-09-23 15:30:12 +0900

    Initial commit

---
+++ src/main/java/dev/RiceCake/RiceCake/RiceCakeApplication.java
@@ -0,0 +1,13 @@
+package dev.RiceCake.RiceCake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiceCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiceCakeApplication.class, args);
+	}
+
+}
