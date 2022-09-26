f369e12ffcd0dc7097170ac4f3eba9c26f1f0553
puzzle527 <puzzle527@naver.com>
2022-09-26 10:43:28 +0900

    add User, StoryList Entity

--- src/main/java/dev/RiceCake/entity/StoryList.java
+++ src/main/java/dev/RiceCake/entity/StoryList.java
@@ -1,4 +1,62 @@
 package dev.RiceCake.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Builder @ToString
+@Entity
+@Table(name = "STORY_LIST")
 public class StoryList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "STORY_LIST_ID")
+    private int storyListID;
+
+    @Column(name = "STORY_LIST_TITLE")
+    private String storyListTitle;
+
+    @Column(name = "STORY_LIST_SUB_TITLE")
+    private String storyListSubTitle;
+
+    @Column(name = "USER_ID")
+    private String userId;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter @Setter
+    @Builder @ToString
+    public static class Request {
+        private String storyListTitle;
+        private String storyListSubTitle;
+        private String userId;
+
+        public static StoryList toEntity(final StoryList.Request request) {
+            return StoryList.builder()
+                    .storyListTitle(request.getStoryListTitle())
+                    .storyListSubTitle(request.getStoryListSubTitle())
+                    .userId(request.getUserId())
+                    .build();
+        }
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter @Setter
+    @Builder @ToString
+    public static class Response {
+        private String storyListTitle;
+        private String storyListSubTitle;
+
+        public static StoryList.Response toResponse(final StoryList storyList) {
+            return StoryList.Response.builder()
+                    .storyListTitle(storyList.getStoryListTitle())
+                    .storyListSubTitle(storyList.getStoryListSubTitle())
+                    .build();
+        }
+    }
 }
--- src/main/java/dev/RiceCake/entity/User.java
+++ src/main/java/dev/RiceCake/entity/User.java
@@ -1,26 +1,74 @@
 package dev.RiceCake.entity;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Builder @ToString
 @Entity
 @Table(name = "USERS")
 public class User {
     @Id
     @Column(name = "USER_ID", length = 100)
-    private String id;
+    private String userId;
 
-    @Column(nullable = false)
+    @Column(name = "PASSWORD", nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(name = "NAME", nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(name = "EMAIL", nullable = false)
     private String email; //이메일 유효성검사
 
+    @Column(name = "STORY_LISTS")
     private List<StoryList> storyLists;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter @Setter
+    @Builder @ToString
+    public static class Request {
+        private String userId;
+        private String password;
+        private String name;
+        private String email;
+
+        public static User toEntity(final Request request) {
+            return User.builder()
+                    .userId(request.getUserId())
+                    .password(request.getPassword())
+                    .name(request.getName())
+                    .email(request.getEmail())
+                    .build();
+        }
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter @Setter
+    @Builder @ToString
+    public static class Response {
+        private String userId;
+        private String name;
+        private String email;
+        private List<StoryList> storyLists;
+
+        public static User.Response toResponse(final User user) {
+            return Response.builder()
+                    .userId(user.getUserId())
+                    .name(user.getName())
+                    .email(user.getEmail())
+                    .storyLists(user.getStoryLists())
+                    .build();
+        }
+
+    }
 }
