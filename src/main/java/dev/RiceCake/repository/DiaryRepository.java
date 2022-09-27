b0329ca130ef3555ef16e80223514ee24bee6bf9
puzzle527 <puzzle527@naver.com>
2022-09-27 19:00:30 +0900

    diaryFinish

--- src/main/java/dev/RiceCake/repository/DiaryRepository.java
+++ src/main/java/dev/RiceCake/repository/DiaryRepository.java
@@ -4,7 +4,9 @@ import dev.RiceCake.entity.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary,Integer> {
-    Diary findById(int diaryId);
+    Diary findByDiaryDate(Date diaryDate);
 }
--- src/main/java/dev/RiceCake/service/DiaryService.java
+++ src/main/java/dev/RiceCake/service/DiaryService.java
@@ -1,13 +1,15 @@
 package dev.RiceCake.service;
 
 import dev.RiceCake.entity.Diary;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 public interface DiaryService {
 
     List<Diary> findAllDiaries();
-    Diary findDiaryById(int diaryId);
+    Diary findDiaryByDate(Date diaryDate);
     Diary saveDiary(Diary newDiary);
     List<Diary> updateDiary(Diary.Request request);
     List<Diary> deleteDiary(int diaryId);
