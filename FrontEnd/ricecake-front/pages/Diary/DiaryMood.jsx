f453023215b61fa046e23ea4fb940c07c624742c
khj1212k <khj1212k@naver.com>
2022-09-29 20:00:00 +0900

    temp commit

---
+++ FrontEnd/ricecake-front/pages/Diary/DiaryMood.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const DiaryMood = () => {
+  return (
+    <div class="container" className="flex">
+      <div class="item" className="border-amber-500">
+        helloflex
+      </div>
+      <div class="item" className="border-amber-500">
+        abc
+      </div>
+      <div class="item" className="border-amber-500">
+        helloflex
+      </div>
+    </div>
+  );
+};
+
+export default DiaryMood;
