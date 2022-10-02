89230bc55547cdd1e0abbbc1a8b629dff2a358af
puzzle527 <puzzle527@naver.com>
2022-10-02 16:14:03 +0900

    Make Users Page

--- FrontEnd/ricecake-front/pages/index.js
+++ FrontEnd/ricecake-front/pages/index.js
@@ -3,12 +3,20 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Starts from '../components/Layout/Starts'
 import Header from '../components/Layout/Header'
+import SignIn from './users/sign-in'
+import SignUp from './users/sign-up'
+import EmailConfirm from './users/email-confirm'
+import FindId from './users/find-id'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Header/>
-      <Starts/>
+    <div className='h-full'>
+      <Header />
+      {/* <Starts/> */}
+      <SignIn />
+      {/* <SignUp /> */}
+      {/* <EmailConfirm /> */}
+      {/* <FindId /> */}
     </div>
   )
 }
